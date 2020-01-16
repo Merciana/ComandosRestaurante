e43e9a9f4cf041ab1eb05294b7ab5a1dbd52d42f
Merciana <53566540+Merciana@users.noreply.github.com>
2020-01-16 12:37:34 -0300

    Add files via upload

---
+++ RestauranteFamoso/src/sample/ControllerPedidos.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ControllerPedidos {
+    @FXML
+    private TableView<?> TableViewPedido;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnMesa;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnPedido;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnGarcom;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnHorario;
+
+    @FXML
+    private ComboBox<?> comBoxPedidoAtual;
+
+    @FXML
+    private ComboBox<?> comboBoxGarcom;
+
+    @FXML
+    private TextArea textAreacomentar;
+
+    @FXML
+    private Button butaoEditarPedido;
+
+    @FXML
+    private Button butaoFecharMesa;
+}
